65ddc33e5a2dd3b9d535efe9b228d1d556c3fb66
FlaShadowK <oarman93sk@gmail.com>
2023-03-18 14:59:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App(){
+  return (
+    <div>
+      <h1>Hello react</h1>
+      <ExpenseItem />
+    </div>
+  );
+} 
+
+export default App;
