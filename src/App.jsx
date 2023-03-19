a831a355f091194729e27d14fc6738ee5f2c5561
FlaShadowK <oarman93sk@gmail.com>
2023-03-19 21:34:45 +0100

    added card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 document.body.style.backgroundColor = 'lightgray';
 
@@ -18,21 +18,7 @@ function App() {
   return (
     <div>
       <h1>Hello react</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
